989afd36988e097303ef6cf480e64bffbd0eca95
Verkerk <floris.verkerk@ordina.nl>
2020-04-21 19:59:53 +0200

    Teg

--- Game.js
+++ Game.js
@@ -1,44 +1,41 @@
 // program data:
-var correctNumber = Math.floor(Math.random() * 100) + 1;
-var userGuess;
-var guessLimit = 5;
-var won = false;
+var number = Math.floor(Math.random() * 100);
+var guess;
+var limit = 5;
+var won =  false;
 var guesses = [];
 
-
-for (i = 1; i <= guessLimit; i++) {
+// game control loop - iterates for each try
+for (var i = 1; i <= limit; i++){
     // prompt user for their guess
     do{
-    userGuess = parseInt(prompt("Guess a number between 1-100"));
-    }
-    while (isPreviousGuess = true);
-     
+        guess = parseInt(prompt("Guess a number between 1-100"));
+    } while(isNaN(guess) || isPreviousGuess(guess));
+    
     // if correct: let the user know they won
-    if(userGuess == correctNumber){
-        window.alert("You've won! The correct number was " + correctNumber);
+    if(guess == number){
+        window.alert("You've won! The correct number was: " + number);
         won = true;
         break;
     }
-    // if incorrect: add number to array and inform user
+    // if incorrect: add number to guesses array
     else{
-        guesses[i] = userGuess;
-        window.alert("Guess is not correct, \n You have guessed: " + guesses.toString() + " please try again \n You have " + (guessLimit - i) + " tries remaining.");
+        guesses[i] = guess;
+        alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
     }
 }
 
 // if the user ran out of tries, let them know the game is over
 if(!won){
-    document.write("Sorry, you ran out of tries. You've only had: " + guessLimit + " tries" +
-    "\n the correct number was: " + correctNumber + ". Please refresh and try again!");
+    window.alert("Sorry, you ran out of tries. You've only had: " + limit + " tries" +
+    "\n the correct number was: " + number + ". Please refresh and try again!");
 }
 
 function isPreviousGuess(){
-    for(i = 0; i < guesses.length; i++){
-        if(guesses[i] = userGuess){
+    for(var i = 1; i < guesses.length; i++){
+        if(guesses[i] == guess){
             return true;
         }
     }
     return false;
 }
-
-
