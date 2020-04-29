57adad7ad37ff04166911732fcadfa5fad4f745f
Verkerk <floris.verkerk@ordina.nl>
2020-04-29 15:38:05 +0200

    Forms

---
+++ Slideshow.js
@@ -0,0 +1,33 @@
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var image = document.getElementById("image");
+
+// store image paths in an array
+var images = ["https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/city.jpg", 
+"https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/cloudy.jpg", 
+"https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/green.jpg", 
+"https://s3-us-west-2.amazonaws.com/s.cdpn.io/1259621/pretty_clouds.jpg"];
+
+var imageIndex = 0;
+// point to previous image when prev button is clicked
+prev.onclick = function(){
+    if(imageIndex == 0){
+        imageIndex = images.length - 1;
+    }
+    else{
+        imageIndex --;
+    }
+    // set the html element "image" to shw the image at the index 
+    image.setAttribute("src", images[imageIndex]);
+}
+// point to previous image when prev button is clicked
+next.onclick = function(){
+    // set image index to 0 when moving past max index
+    if(imageIndex == images.length - 1){
+      imageIndex = 0;
+    }
+    else{
+      imageIndex++;
+    }
+    image.setAttribute("src", images[imageIndex]);
+  }
